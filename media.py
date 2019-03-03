aab4d978725d56525feb3951dfa9c6d118046652
Atul Makam <atul1makam@gmail.com>
2019-03-03 22:41:44 +0530

    Add files via upload

---
+++ media.py
@@ -0,0 +1,19 @@
+import webbrowser
+class Movie():
+    
+    def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
+        """ movie name """
+        self.title = movie_title
+        """ movie storyline """
+        self.storyline = movie_storyline
+        """ movie poster image url to access """
+        self.poster_image_url = poster_image
+        """ movie trailer url to access """
+        self.trailer_youtube_url = trailer_youtube
+     
+    def show_trailer(self):
+        """self - refers to a variable field within the class
+        this function show_trailer(self) shows up trailer
+        self is used as a strong convention"""
+        webbrowser.open(self.trailer_youtube_url)
+
