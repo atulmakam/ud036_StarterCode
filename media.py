8c504611742dbde77108f3bb2037673fe2809f09
Atul Makam <atul1makam@gmail.com>
2019-03-05 01:26:14 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,18 +1,19 @@
 import webbrowser
-class Movie():
-    
+
+
+class Movie():    
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        """ movie name """
+        """ movie name"""
         self.title = movie_title
-        """ movie storyline """
+        """movie storyline"""
         self.storyline = movie_storyline
-        """ movie poster image url to access """
+        """movie poster image url to access"""
         self.poster_image_url = poster_image
-        """ movie trailer url to access """
-        self.trailer_youtube_url = trailer_youtube
-     
+        """movie trailer url to access"""
+        self.trailer_youtube_url = trailer_youtube   
+        
     def show_trailer(self):
-        """self refers to a variable field within the class
+        """self refers to a variable field within the class 
         this function shows the movie trailer"""
         webbrowser.open(self.trailer_youtube_url)
 
