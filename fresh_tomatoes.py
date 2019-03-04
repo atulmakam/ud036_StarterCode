3591c75b9831f57e587ec26957f6ea88fcdef420
Atul Makam <atul1makam@gmail.com>
2019-03-04 23:04:08 +0530

    Update fresh_tomatoes.py

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -2,11 +2,8 @@ import webbrowser
 import os
 import re
 
-
 # Styles and scripting for the page
 main_page_head = '''
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="utf-8">
     <title>Fresh Tomatoes!</title>
@@ -56,6 +53,19 @@ main_page_head = '''
             top: 0;
             background-color: white;
         }
+        .storyline{
+            display: none;
+            position: absolute;
+            background-color: rgba(255,255,255,0.9);
+            color: #000;
+            bottom: 0px;
+            top: 75%;
+            left: 0;
+            width: 100%;
+            text-align: left;
+            padding: 20px 40px;
+            font-size: 20px;
+        }
     </style>
     <script type="text/javascript" charset="utf-8">
         // Pause the video when the modal is closed
@@ -80,14 +90,22 @@ main_page_head = '''
           $('.movie-tile').hide().first().show("fast", function showNext() {
             $(this).next("div").show("fast", showNext);
           });
+
+          $(".movie-tile").hover(function(){
+              $(this).children(".storyline").slideDown();
+            }, function(){
+              $(this).children(".storyline").slideUp();
+            }
+          );
         });
     </script>
 </head>
 '''
 
-
 # The main page layout and title bar
 main_page_content = '''
+<!DOCTYPE html>
+<html lang="en">
   <body>
     <!-- Trailer Video Modal -->
     <div class="modal" id="trailer">
@@ -101,7 +119,7 @@ main_page_content = '''
         </div>
       </div>
     </div>
-
+    
     <!-- Main Page Content -->
     <div class="container">
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -119,12 +137,14 @@ main_page_content = '''
 </html>
 '''
 
-
 # A single movie entry html template
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
+    <div class="storyline">
+      {storyline}
+    </div>
 </div>
 '''
 
@@ -134,18 +154,16 @@ def create_movie_tiles_content(movies):
     content = ''
     for movie in movies:
         # Extract the youtube ID from the url
-        youtube_id_match = re.search(
-            r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
-        youtube_id_match = youtube_id_match or re.search(
-            r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
-        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
-                              else None)
+        youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
+        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+        trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
+            storyline=movie.storyline
         )
     return content
 
@@ -154,14 +172,13 @@ def open_movies_page(movies):
     # Create or overwrite the output file
     output_file = open('fresh_tomatoes.html', 'w')
 
-    # Replace the movie tiles placeholder generated content
-    rendered_content = main_page_content.format(
-        movie_tiles=create_movie_tiles_content(movies))
+    # Replace the placeholder for the movie tiles with the actual dynamically generated content
+    rendered_content = main_page_content.format(movie_tiles=create_movie_tiles_content(movies))
 
     # Output the file
     output_file.write(main_page_head + rendered_content)
     output_file.close()
 
-    # open the output file in the browser (in a new tab, if possible)
+    # open the output file in the browser
     url = os.path.abspath(output_file.name)
-    webbrowser.open('file://' + url, new=2)
+    webbrowser.open('file://' + url, new=2) # open in a new tab, if possible
